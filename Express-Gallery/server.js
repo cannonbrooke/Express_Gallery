79afdf1ba9e3c92ffc8249e49600538038506f3a
Cannon Brooke <cannonbrooke@protonmail.ch>
2015-07-27 17:17:52 -1000

    got routes to work

---
+++ Express-Gallery/server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+
+var config = require('./config/config.json');
+var app = express();
+
+//var session = require('express-session');
+
+var bodyparser = require('body-parser');
+var path = require('path');
+
+//app.use(bodyParser.urlencoded());
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.get('/', function (req, res){
+  res.render('new');
+});
+
+app.listen(3000, function(){
+});
+
+//var server = app.listen(config.port, displayServer);
